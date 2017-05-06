cac79091dbfab174e2deb52fe83cc1fdd339c339
Taras Ignashchenko <tignashchenko@gmail.com>
2017-05-06 16:17:20 -0700

    implement tests for _pluck function

--- test/pluck.test.js
+++ test/pluck.test.js
@@ -11,4 +11,23 @@ describe('pluck()', () => {
     expect(result).toEqual([12, 999, 14]);
   });
 
+  it('returns an array of just occupations, given an array of people objects', () => {
+    const people = [
+      { name: 'Taras', occupation: 'HR student'},
+      { name: 'Bogdan', occupation: 'Private equity principal'},
+      { name: 'Olga', occupation: 'Philosophy professor'}
+    ];
+    const result = _.pluck(people, 'occupation');
+    expect(result).toEqual(['HR student', 'Private equity principal', 'Philosophy professor']);
+  });
+
+  it('returns an array of just employment statuses, given an array of people objects', () => {
+    const people = [
+      { name: 'Taras', employed: false},
+      { name: 'Bogdan', employed: true},
+      { name: 'Olga', employed: true}
+    ];
+    const result = _.pluck(people, 'employed');
+    expect(result).toEqual([false, true, true]);
+  });   
 });
