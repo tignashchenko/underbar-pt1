c57ada6d8693e55b21dd62332f2d2ab2ee0963d9
Taras Ignashchenko <tignashchenko@gmail.com>
2017-05-06 15:35:35 -0700

    implement tests for _map function

--- test/map.test.js
+++ test/map.test.js
@@ -6,4 +6,22 @@ describe('map()', () => {
     const mappedArr = _.map(arr, (el) => el * el);
     expect(mappedArr).toEqual([1, 4, 9, 16, 25]);
   });
+
+  it('maps every word in an array of strings with an exclamation mark', () => {
+    const arr = ['Taras', 'Tim', 'Kay'];
+    const mappedArr = _.map(arr, (el) => el + '!');
+    expect(mappedArr).toEqual(['Taras!', 'Tim!', 'Kay!']);
+  });
+
+  it('return all array elements intact if no callback is passed', () => {
+    const arr = ['Taras', 'Tim', 'Kay'];
+    const mappedArr = _.map(arr);
+    expect(mappedArr).toEqual(arr);
+  });
+
+  it('map string element to its reverse', () => {
+    const arr = ['San Francisco'];
+    const mappedArr = _.map(arr, (el) => el.split('').reverse().join(''));
+    expect(mappedArr).toEqual(['ocsicnarF naS']);
+  });
 });
