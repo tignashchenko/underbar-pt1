34c8b33beaf56d9caa3986ba78dfa53dece36643
Taras Ignashchenko <tignashchenko@gmail.com>
2017-05-07 15:18:09 -0700

    implement tests for _uniq function

--- test/uniq.test.js
+++ test/uniq.test.js
@@ -5,4 +5,19 @@ describe('uniq()', () => {
     const nums = [2, 4, 5, 5, 7, 4, 10, 2];
     expect(_.uniq(nums)).toEqual([2, 4, 5, 7, 10]);
   });
+
+  it('de-dups a list of booleans', () => {
+    const bools = [true, false, true, true, false, false, true];
+    expect(_.uniq(bools)).toEqual([true, false]);
+  });
+
+  it('de-dups an object of keys', () => {
+    const tarasObj = {
+      name: 'Some unknown nickname',
+      name: 'Taras',
+      age: 'much less than 26',
+      age: 26
+    };
+    expect(_.uniq(tarasObj)).toEqual(['Taras', 26]);
+  });
 });
