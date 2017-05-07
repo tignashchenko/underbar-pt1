16c21a6938c0ed523d38fb27aa8d25eabeffeb61
Taras Ignashchenko <tignashchenko@gmail.com>
2017-05-07 14:37:10 -0700

    implement tests for _reduce function

--- test/reduce.test.js
+++ test/reduce.test.js
@@ -13,4 +13,16 @@ describe('reduce()', () => {
     expect(result).toEqual(15);
   });
 
+  it('count occurences of number to an object', () => {
+    const numObj = [1, 3, 5, 2, 1, 5, 7, 7, 3];
+    const result = _.reduce(numObj, function(acc, curr) {
+      if(curr in acc) {
+        ++acc[curr];
+      } else {
+        acc[curr] = 1;
+      }
+    return acc;
+  }, {});
+  expect(result).toEqual({'1': 2, '2': 1, '3': 2, '5': 2, '7':2});
+});
 });
