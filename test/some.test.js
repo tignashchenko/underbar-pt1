3685878c7e9d8f671e08a863b06bfa3f838e0912
Taras Ignashchenko <tignashchenko@gmail.com>
2017-05-07 14:54:15 -0700

    implement tests for _some function

--- test/some.test.js
+++ test/some.test.js
@@ -11,4 +11,13 @@ describe('some()', () => {
     expect(_.some(nums, num => num % 2 === 1)).toBe(false);
   });
 
+  it('returns true if at least one of the elements is of type boolean', () => {
+    const randomArray = [30, null, false, 'Hack Reactor', 'San Francisco', true];
+    expect(_.some(randomArray, element => typeof(element) === 'boolean')).toBe(true);
+  });
+
+  it('returns false if all string have a length of greater than 5', () => {
+    const stringArray = ['TarasI', 'California', 'Ukraine', 'Hack Reactor'];
+    expect(_.some(stringArray, str => str.length <= 5)).toBe(false);
+  });
 });
